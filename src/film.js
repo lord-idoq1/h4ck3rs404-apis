a05e98ec9d4f8da6e6c18d1592c66623d557fcc2
lord-idoq1 <90918779+lord-idoq1@users.noreply.github.com>
2021-11-13 10:47:37 +0700

    Update film.js

--- src/film.js
+++ src/film.js
@@ -1,5 +1,5 @@
 const fetch = require('node-fetch')
-const baseURI = 'https://h4ck3rs404-api.herokuapp.com/'
+const baseURI = 'https://xtream-api.herokuapp.com/'
 
 module.exports.indoxxi = function indoxxi(q) {
 	return new Promise((resolve, reject) => {
