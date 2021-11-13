1ca791d31d8074db1284915a2f0aff2ba0d889fb
lord-idoq1 <90918779+lord-idoq1@users.noreply.github.com>
2021-11-13 10:45:56 +0700

    Update textpro.js

--- src/textpro.js
+++ src/textpro.js
@@ -1,5 +1,5 @@
 const bent = require('bent')
-const baseURI = 'https://h4ck3rs404-api.herokuapp.com/'
+const baseURI = 'https://xtream-api.herokuapp.com/'
 
 module.exports.blackpink = function blackpink(text1) {
 	return new Promise((resolve, reject) => {
